485fee3df8f229e67b0a5071f66cbfaa3d7462c7
sora083 <sora.tomita083@gmail.com>
2022-02-13 12:41:38 +0900

    Add TODO element

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
-import "./styles.css";
+import './styles.css';
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  const inputText = document.getElementById('add-text').value;
+  document.getElementById('add-text').value = ''; // テキストの値を初期化
+
+  // divタグ生成
+  const div = document.createElement('div');
+  div.className = 'list-row';
+
+  // liタグ生成
+  const li = document.createElement('li');
+  li.innerText = inputText;
+
+  // buttonタグの生成
+  const completeButton = document.createElement('button');
+  completeButton.innerText = '完了';
+  completeButton.addEventListener('click', () => {
+    alert('完了');
+  });
+
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = '削除';
+  deleteButton.addEventListener('click', () => {
+    alert('削除');
+  });
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了のリストに追加
+  document.getElementById('incomplete-list').appendChild(div);
+};
+
+document
+  .getElementById('add-button')
+  .addEventListener('click', () => onClickAdd());
